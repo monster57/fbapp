5bcbb43d75a874e7b5fed62f22fc8f3ca5f8376f
monster57 <surajitbarman57@yahoo.in>
2016-04-27 16:54:43 +0530

    database setup is done

---
+++ models/user.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    name:{
+      type: DataTypes.STRING,
+      field:'name'
+    },
+    password:{
+      type:DataTypes.STRING,
+      field:'password'
+    },
+    role:{
+      type:DataTypes.STRING,
+      field:'role',
+      defaultValue:'user'
+    },
+    profileimage:{
+      type:DataTypes.STRING,
+      field:'profileimage'
+    }
+  },{
+    tableName: 'users'
+  });
+
+  return User;
+};
