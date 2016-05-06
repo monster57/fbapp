fef91d75054375a1088638a12730838b5c9dfc9d
Vijaya <vijaya@teampumpkin.com>
2016-05-06 18:47:54 +0530

    re-edited the login page is done alone with new style sheet is added(fb.css). To new background image are added to image folder

--- models/user.js
+++ models/user.js
@@ -12,6 +12,10 @@ if(config().url == 'mongodb://localhost/nodeauthtest'){
 }
 
 var db = mongoose.connection;
+db.on('error', function(err) { console.log(err.message); });
+db.once('open', function() {
+  console.log("mongodb connection open");
+});
 
 // User Schema
 var UserSchema = mongoose.Schema({
